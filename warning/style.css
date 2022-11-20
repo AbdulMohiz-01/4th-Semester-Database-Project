7b23da86e8c1b1b0dd492be8f1a15d5b379e6d84
AbdulMohiz-01 <FA20-BCS-001@cuiatd.edu.pk>
2022-11-20 12:59:54 +0500

    This is initail commit

---
+++ warning/style.css
@@ -0,0 +1,42 @@
+* {
+  font-family:"Raleway";
+}
+#notification-area {
+  position:fixed;
+  top:0px;
+  right:10px;
+  width:250px;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-end;
+}
+#notification-area .notification {
+  position:relative;
+  padding:15px 10px;
+  background:#111;
+  color:#f5f5f5;
+  font-family:"Raleway";
+  font-size:14px;
+  font-weight:600;
+  border-radius:5px;
+  margin:5px 0px;
+  opacity:0;
+  left:20px;
+  animation:showNotification 500ms ease-in-out forwards;
+}
+@keyframes showNotification {
+  to {
+    opacity:1;
+    left:0px;
+  }
+}
+#notification-area .notification.success {
+  background:#389838;
+}
+#notification-area .notification.error {
+  background:orangered;
+}
+#notification-area .notification.info {
+  background:#00acee;
+}
